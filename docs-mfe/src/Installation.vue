5ea83c67e04113a9c965a5a9f03c05e46b46914f
Maniero <cmaniero@thoughtworks.com>
2020-10-20 22:43:41 -0300

    docs-mfe vue project

---
+++ docs-mfe/src/Installation.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="installation">
+    <img alt="Vue logo" v-bind:src="logo">
+
+    <h1>Welcome to ragu-vue-server-adapter documentation!</h1>
+    <div>
+      <p>
+        <strong>ragu-vue-server-adapter</strong> delivers an abstraction on top of <strong>ragu-server</strong> to
+        enable a straightforward way to deliver your Vue.js components as micro-frontends.
+      </p>
+
+      <h2>About this documentation</h2>
+
+      <p>This documentation is actually a micro-frontend wrote in vue using this adapter. Isn't is insane?</p>
+
+      <ul>
+        <li><strong>Micro-frontend</strong> <a href="http://ragu-vue-server-adapter.herokuapp.com/components/installation" target="_blank">http://ragu-vue-server-adapter.herokuapp.com/components/installation</a>.</li>
+        <li><strong>Preview</strong> <a href="http://ragu-vue-server-adapter.herokuapp.com/preview/installation" target="_blank">http://ragu-vue-server-adapter.herokuapp.com/preview/installation</a></li>
+        <li><strong>Repository</strong> <a href="https://github.com/ragu-framework/ragu-vue-server-adapter/tree/main/docs-mfe" target="_blank">https://github.com/ragu-framework/ragu-vue-server-adapter/tree/main/docs-mfe</a></li>
+      </ul>
+
+      Go through the next steps and check it out how easy it is to expose your components as micro-frontends.
+    </div>
+  </div>
+</template>
+
+<script>
+import logo from '../../repository-assets/logo.png'
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      logo
+    };
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.installation {
+  font-family: var(--default-font-family, 'Poppins', sans-serif);
+  font-weight: 300;
+  font-size: 0.875rem;
+  line-height: 1.375;
+}
+
+h1 {
+  font-family: var(--secondary-title-font-family, Lato, sans-serif);
+  font-weight: var(--secondary-title-weight, 300);
+  font-size: 1.5rem;
+}
+
+h2 {
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 1.625rem;
+  font-family: var(--default-font-family, 'Poppins', sans-serif);
+}
+
+a {
+  color: #BF265E;
+}
+</style>
---
+++ docs-mfe/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from 'docs-mfe/src/Installation.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
