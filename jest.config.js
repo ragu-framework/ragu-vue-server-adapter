e89f88324f88d8bc3e2d48830b1f139cd7fbe38e
Maniero <carlosmaniero@gmail.com>
2021-03-06 18:11:49 -0300

    increases the timeout

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 30000,
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
   coveragePathIgnorePatterns: [
     "/node_modules/",
