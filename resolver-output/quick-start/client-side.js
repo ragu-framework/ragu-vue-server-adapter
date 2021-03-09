9cd23192ee910df0927089dae1dd248e65b0614d
carlosmaniero <carlosmaniero@gmail.com>
2021-03-09 03:25:30 +0000

    Deploying to gh-pages from  @ 943178d1aebb9872365a0960207af930c1b2fdea 🚀

--- resolver-output/quick-start/client-side.js
+++ resolver-output/quick-start/client-side.js
@@ -1,5 +1,5 @@
 
-      var component = require('/home/runner/work/ragu-vue-server-adapter/ragu-vue-server-adapter/docs-mfe/ragu-components/installation/index');
+      var component = require('/home/runner/work/ragu-vue-server-adapter/ragu-vue-server-adapter/docs-mfe/ragu-components/quick-start');
       var resolver = require('/home/runner/work/ragu-vue-server-adapter/ragu-vue-server-adapter/docs-mfe/node_modules/ragu-vue-server-adapter/resolvers/hydrate-resolver');
 
       module.exports.default = (resolver.default || resolver)(component.default || component);
